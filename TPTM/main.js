92e29af1bce05b9be23b98ba33587c29d8099cf3
iwasaku <hgh02676+github@gmail.com>
2022-06-22 23:33:49 +0900

    実験33

--- TPTM/main.js
+++ TPTM/main.js
@@ -170,7 +170,7 @@ tm.define("TitleScene", {
                     fillStyle: "#fff",
                     fontSize: 160,
                     fontFamily: FONT_FAMILY,
-                    text: "TPTM\n32",
+                    text: "TPTM\n33",
                     align: "center",
                 },
                 {
